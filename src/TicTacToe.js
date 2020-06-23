095dce94936c64b1d4e246a920acc64c1b28a201
jwfrasier <frasier.joseph@gmail.com>
2020-06-23 08:34:03 -0500

    removed comments

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -10,47 +10,33 @@ function TicTacToe() {
 
 export default TicTacToe;
 
-// container
-// value (prop)
-// onClick function (prop)
 function Square(props) {
   return <button onClick={props.onClick}>{props.value}</button>;
 }
 
-// board
 function Board() {
-  // state
-  //board state
   const [boardSquares, setBoardSquares] = useState(Array(9).fill(null));
-  // turn state
-  const [xIsNext, setXIsNext] = useState(true);
-  // handleclick
-  const handleClick = index => {
-    // copy of our board state
-    const newBoardSquares = [...boardSquares];
-    // if the index of the board is filled, return
-    if (determineWinner(boardSquares) || newBoardSquares[index]) return;
-    // mutate the copy, and add x or o
-    newBoardSquares[index] = xIsNext ? "X" : "O";
-    //calc next turn
 
-    // set the state of the board
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = index => {
+    const newBoardSquares = [...boardSquares];
+
+    if (determineWinner(boardSquares) || newBoardSquares[index]) return;
+
+    newBoardSquares[index] = xIsNext ? "X" : "O";
+
     setBoardSquares(newBoardSquares);
-    // set the state of the next turn
+
     setXIsNext(!xIsNext);
   };
 
-  // create our board
-
-  // create a render square function
   const renderSquare = index => {
     return (
       <Square value={boardSquares[index]} onClick={() => handleClick(index)} />
     );
   };
-  // take in an index
-  // return a square, with the correct value and function
-  // initialize status
+
   let status;
   const winner = determineWinner(boardSquares);
 
@@ -78,9 +64,8 @@ function Board() {
     </div>
   );
 }
-// function to calc winner
+
 function determineWinner(squares) {
-  // get our set of winning lines
   const winningLines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -91,7 +76,7 @@ function determineWinner(squares) {
     [0, 4, 8],
     [2, 4, 6]
   ];
-  // loop through set
+
   for (let i = 0; i < winningLines.length; i++) {
     const [a, b, c] = winningLines[i];
     if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
@@ -99,7 +84,4 @@ function determineWinner(squares) {
     }
   }
   return null;
-  // check if values in our squares array are winning lines
-  //if so, return X or O
-  // else return nothing
 }
