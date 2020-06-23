9a959b86c72345fabbf062c9e0ede6431eb17909
jwfrasier <frasier.joseph@gmail.com>
2020-06-23 14:19:54 -0500

    Fixed layout for buttons and title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import TicTacToe from "./TicTacToe";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 100px 500px 100px;
+  grid-template-rows: auto;
+  grid-gap: 5px;
+  justify-content: center;
+  text-align: center;
+`;
 
 function App() {
   return (
     <div className="App">
-      <TicTacToe />
+      <Container>
+        <TicTacToe />
+      </Container>
     </div>
   );
 }
--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,21 +1,59 @@
 import React, { useReducer, useMemo } from "react";
 import { gameReducer, initialGameState } from "./gameReducer";
+import styled from "styled-components";
+
+export const Row = styled.div`
+  height: 64px;
+  display: flex;
+`;
+
+export const Button = styled.button`
+  height: 64px;
+  width: 64px;
+  font-size: 10px;
+  appearance: none;
+  background: transparent;
+  border: 1px solid gray;
+  padding: 0;
+  margin: 0;
+  text-decoration: none;
+
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    border-color: black;
+  }
+`;
+
+export const TitleContainer = styled.div`
+  grid-column: 2/2;
+`;
+
+export const ButtonContainer = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  grid-column: 2/2;
+  grid-row: 2/2;
+`;
+
+export const TicTacToeContainer = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  grid-column: 2/3;
+  grid-row: 3/8;
+`;
 
 function TicTacToe() {
-  return (
-    <div className="TicTacToe">
-      <Board />
-    </div>
-  );
+  return <Board />;
 }
 
 export default TicTacToe;
 
 function Square(props) {
   return (
-    <button style={{ height: "36px" }} onClick={props.onClick}>
-      {props.value}
-    </button>
+    <Button style={{ fontSize: "24px" }} onClick={props.onClick}>
+      {props.value || " "}
+    </Button>
   );
 }
 
@@ -42,68 +80,89 @@ function Board() {
   const renderSquare = renderSquareWithReducer(state, dispatch);
 
   return (
-    <div>
-      <div>{title}</div>
-      <div>O Wins: {state.playerOWins}</div>
-      <div>X Wins: {state.playerXWins}</div>
-      <div>
-        <button onClick={() => dispatch({ type: "rematch" })}>Rematch</button>
-        <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
-        <button onClick={() => dispatch({ type: "easy" })}>
-          Easy Mode (3x3)
-        </button>
-        <button onClick={() => dispatch({ type: "hard" })}>
-          Hard Mode (4x4)
-        </button>
-      </div>
+    <React.Fragment>
+      <TitleContainer className="titleContainer">
+        <div className="title">{title}</div>
+        <div className="Owins">O Wins: {state.playerOWins}</div>
+        <div className="Xwins">X Wins: {state.playerXWins}</div>
+      </TitleContainer>
+
+      <ButtonContainer className="ButtonContainer">
+        <Row>
+          <Button
+            className="headerbutton"
+            onClick={() => dispatch({ type: "rematch" })}
+          >
+            Rematch
+          </Button>
+          <Button
+            className="headerbutton"
+            onClick={() => dispatch({ type: "reset" })}
+          >
+            Reset
+          </Button>
+          <Button
+            className="headerbutton"
+            onClick={() => dispatch({ type: "easy" })}
+          >
+            Easy Mode (3x3)
+          </Button>
+          <Button
+            className="headerbutton"
+            onClick={() => dispatch({ type: "hard" })}
+          >
+            Hard Mode (4x4)
+          </Button>
+        </Row>
+      </ButtonContainer>
       {state.difficulty === "easy" && (
-        <div>
-          <div>
+        <TicTacToeContainer className="tictactoeccontainer">
+          <Row>
             {renderSquare(0)}
             {renderSquare(1)}
             {renderSquare(2)}
-          </div>
+          </Row>
 
-          <div>
+          <Row>
             {renderSquare(3)}
             {renderSquare(4)}
             {renderSquare(5)}
-          </div>
-          <div>
+          </Row>
+          <Row>
             {renderSquare(6)}
             {renderSquare(7)}
             {renderSquare(8)}
-          </div>
-        </div>
+          </Row>
+        </TicTacToeContainer>
       )}
       {state.difficulty === "hard" && (
-        <div>
-          <div>
+        <TicTacToeContainer className="tictactoeccontainer">
+          <Row>
             {renderSquare(0)}
             {renderSquare(1)}
             {renderSquare(2)}
             {renderSquare(3)}
-          </div>
-          <div>
+          </Row>
+          <Row>
             {renderSquare(4)}
             {renderSquare(5)}
             {renderSquare(6)}
             {renderSquare(7)}
-          </div>
-          <div>
+          </Row>
+          <Row>
             {renderSquare(8)}
             {renderSquare(9)}
             {renderSquare(10)}
             {renderSquare(11)}
-          </div>
-          <div>
+          </Row>
+          <Row>
             {renderSquare(12)}
             {renderSquare(13)}
             {renderSquare(14)}
             {renderSquare(15)}
-          </div>
-        </div>
+          </Row>
+        </TicTacToeContainer>
       )}
-    </div>
+    </React.Fragment>
   );
 }
